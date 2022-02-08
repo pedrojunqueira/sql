c122c92ce5aec018d32b2d199453072c592871a8
Pedro Junqueira <pedrocj@gmail.com>
2022-02-09 05:23:01 +1030

    more sql

--- sql_cookbook/chap05/5.6_sql_generate_sql.sql
+++ sql_cookbook/chap05/5.6_sql_generate_sql.sql
@@ -1,3 +1,13 @@
 select FORMATMESSAGE('count(1) from %s', table_name)
 from INFORMATION_SCHEMA.TABLES
 where TABLE_SCHEMA = 'dbo'
+
+
+---------------------------
+
+select 'insert into table_name ( empno, ename, mgr, hiredate, sal, comm, deptno )' 
+
++ FORMATMESSAGE('    values (%d )', empno )
+from emp where empno = 7369
+
+
