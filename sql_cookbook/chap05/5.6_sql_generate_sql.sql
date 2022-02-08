0160ed7cf79d1071fd1bdf06669fbec07fc62b91
Pedro Junqueira <pedrocj@gmail.com>
2022-02-09 05:43:24 +1030

    more sql self generated

--- sql_cookbook/chap05/5.6_sql_generate_sql.sql
+++ sql_cookbook/chap05/5.6_sql_generate_sql.sql
@@ -4,10 +4,18 @@ where TABLE_SCHEMA = 'dbo'
 
 
 ---------------------------
+-- populate a copy of the data of a table --
 
-select 'insert into table_name ( empno, ename, mgr, hiredate, sal, comm, deptno )' 
+select 'insert into emp ( empno, ename, mgr, hiredate, sal, comm, deptno )' ,
+FORMATMESSAGE('values (%s, ''%s'', %s, ''%s'', %s, %s, %s)'
+, coalesce (cast(empno as varchar), 'NULL')
+, coalesce (ename, 'NULL')
+, coalesce( cast(mgr as varchar), 'NULL')
+, coalesce( cast(hiredate as varchar), 'NULL')
+, coalesce (cast(sal as varchar), 'NULL')
+, coalesce (cast(comm as varchar), 'NULL')
+, coalesce( cast(deptno as varchar), 'NULL')
+)
 
-+ FORMATMESSAGE('    values (%d )', empno )
-from emp where empno = 7369
-
+from emp 
 
